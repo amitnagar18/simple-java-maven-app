80510483ec7c7ffcb4bede3ac7a92afbdc858410
Nagar <amit.nagar01@sap.com>
2018-08-20 16:04:34 +0200

    changed file to test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,8 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+        System.out.println("Chnaged the code");
+        
     }
 
     private final String getMessage() {
